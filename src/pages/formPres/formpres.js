4d2feb88175b9fa2c0439bdc9d4af381564cf5db
Isaac Flores <isaacfloresva@gmail.com>
2023-08-07 08:41:16 -0600

    Update formpres.js

--- src/pages/formPres/formpres.js
+++ src/pages/formPres/formpres.js
@@ -1397,7 +1397,6 @@ const CompFormpres = () => {
         <div className="container">
           <div className="row">
             <div className="col-md-6">              
-              <button type="submit" disabled className="d-none"></button>
               <h3 className="clrTitle">Datos del registro </h3>
             </div>
             <div className="col-md-6">
