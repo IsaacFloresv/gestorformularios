3e8caba845483f1d28530bad870dd3b9da5c5317
Isaac Flores <jordyngv14@outlook.com>
2025-05-18 21:18:22 -0600

    Update server.js

--- server.js
+++ server.js
@@ -2,20 +2,14 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-// Servir archivos estáticos
 app.use(express.static(path.join(__dirname, './')));
 
-// Endpoint raíz
 app.get('/', function (req, res) {
-res.sendFile(path.join(__dirname, './', 'index.html'));
+  res.sendFile(path.join(__dirname, './', 'index.html'));
 });
 
-// Endpoint /health para que Railway verifique que el servidor está activo
 app.get('/health', (req, res) => {
-res.status(200).send('OK');
+  res.status(200).send('OK - Application is running');
 });
 
-// Puerto de escucha
-app.listen(9000, () => {
-console.log('Servidor escuchando en puerto 9000');
-});
+app.listen(9000);
